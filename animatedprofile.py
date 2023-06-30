4fe5e6ee851f0c8d0832c6c1e8aee97521ad7381
Alexey <107789010+sqlmerr@users.noreply.github.com>
2023-06-30 09:39:44 +0300

    Update animatedprofile.py
    1.0.0

--- animatedprofile.py
+++ animatedprofile.py
@@ -9,10 +9,9 @@
 # Description: Модуль для анимации вашего профиля (имя, био)
 # Author: sqlmerr
 # Commands:
-# .animatedname (.aname) | .animatedbio (.abio) | .animatedstopname (.astopname) | .animatedstopbio (.astopbio)
+# .animatedname (.aname) | .animatedbio (.abio) | .stopanimatedname (.stopaname) | .stopanimatedbio (.stopabio)
 # ---------------------------------------------------------------------------------------------
 
-# версия модуля
 __version__ = (1, 0, 0)
 # meta developer: @sqlmerr_m
 # only hikka
@@ -35,15 +34,33 @@ logger = logging.getLogger(__name__)
 # сам класс модуля
 @loader.tds
 class AnimatedProfile(loader.Module):
-    """Модуль для анимации вашего профиля (имя, био)"""
+    """Module for your profile animation (name, bio) | Модуль для анимации вашего профиля (имя, био)"""
     strings = {
-        "name": "AnimatedProfile",
-        "name_status": "Включена ли анимация имени или нет",
+        "name" : "AnimatedProfile",
+        "name_delay": "Time between frames of name animation",
+        "animated_name_frames": "Name animation frames",
+        "name_is_enabled": "<emoji document_id=5447644880824181073>⚠️</emoji> Name animation is already enabled, use <code>.astopname</code> to turn it off.",
+        "name_is_disabled": "<emoji document_id=5447644880824181073>⚠️</emoji> Name animation is already turned off.",
+        "name_turned_off": "<emoji document_id=5447644880824181073>⚠️</emoji> Name animation is disabled.",
+        "bio_status": "Is the bio animation enabled or not",
+        "bio_delay": "Time between frames of bio animation",
+        "animated_bio_frames": "Bio animation frames",
+        "bio_is_enabled": "<emoji document_id=5447644880824181073>⚠️</emoji> Bio animation is already enabled, use <code>.astopname</code> to turn it off.",
+        "bio_is_disabled": "<emoji document_id=5447644880824181073>⚠️</emoji> Bio animation is already turned off.",
+        "bio_turned_off": "<emoji document_id=5447644880824181073>⚠️</emoji> Bio animation is disabled.",
+    }
+    strings_ru = {
         "name_delay": "Время между кадрами анимации имени",
         "animated_name_frames": "Кадры анимации имени",
+        "name_is_enabled": "<emoji document_id=5447644880824181073>⚠️</emoji> Анимация имени уже включена, используйте <code>.stopaname</code>, чтобы выключить.",
+        "name_is_disabled": "<emoji document_id=5447644880824181073>⚠️</emoji> Анимация имени уже выключена.",
+        "name_turned_off": "<emoji document_id=5447644880824181073>⚠️</emoji> Анимация имени выключена.",
         "bio_status": "Включена ли анимация био или нет",
         "bio_delay": "Время между кадрами анимации био",
         "animated_bio_frames": "Кадры анимации био",
+        "bio_is_enabled": "<emoji document_id=5447644880824181073>⚠️</emoji> Анимация био уже включена, используйте <code>.stopabio</code>, чтобы выключить.",
+        "bio_is_disabled": "<emoji document_id=5447644880824181073>⚠️</emoji> Анимация био уже выключена.",
+        "bio_turned_off": "<emoji document_id=5447644880824181073>⚠️</emoji> Анимация био выключена.",
     }
 
     def __init__(self):
@@ -76,55 +93,45 @@ class AnimatedProfile(loader.Module):
             ),
         )
 
-    @loader.command(alias="aname")
+    @loader.command(alias="aname", ru_doc="""(aname) Включить анимацию имени""")
     async def animatedname(self, message: Message):
-        """(aname) Включить анимацию имени"""
-        if not self.aname:
+        """(aname) Turn on name animation"""
+        if self.aname is False:
             self.aname = True
             await message.delete()
             while self.aname:
-                if self.aname:
-                    for n in self.config["animated_name_frames"]:
-                        await asyncio.sleep(self.config["name_delay"])
-                        await self.client(functions.account.UpdateProfileRequest(first_name=n))
-                else:
-                    return
-                    break
+                for n in self.config["animated_name_frames"]:
+                    await asyncio.sleep(self.config["name_delay"])
+                    await self.client(functions.account.UpdateProfileRequest(first_name=n))
         else:
-            return await utils.answer(message, "<emoji document_id=5447644880824181073>⚠️</emoji> Анимация имени уже включена, используйте <code>.astopname</code>, чтобы выключить.")
+            return await utils.answer(message, self.strings("name_is_enabled"))
 
 
-    @loader.command(alias="abio")
+    @loader.command(alias="abio", ru_doc="""(abio) Включить анимацию био""")
     async def animatedbio(self, message: Message):
-        """(abio) Включить анимацию био"""
-        if not self.abio:
+        """(abio) Turn on bio animation"""
+        if self.abio is False:
             self.abio = True
             await message.delete()
             while self.abio:
-                if self.abio:
-                    for n in self.config["animated_bio_frames"]:
-                        await asyncio.sleep(self.config["bio_delay"])
-                        await self.client(functions.account.UpdateProfileRequest(about=n))
-                else:
-                    return
-                    break
+                for n in self.config["animated_bio_frames"]:
+                    await asyncio.sleep(self.config["bio_delay"])
+                    await self.client(functions.account.UpdateProfileRequest(about=n))
         else:
-            return await utils.answer(message, "<emoji document_id=5447644880824181073>⚠️</emoji> Анимация био уже включена, используйте <code>.astopbio</code>, чтобы выключить.")
+            return await utils.answer(message, self.strings("bio_is_enabled"))
 
-    @loader.command(alias="astopname")
-    async def animatedstopname(self, message: Message):
-        """(astopname) Выключить анимацию"""
-        if not self.aname:
-            return await utils.answer(message, "<emoji document_id=5447644880824181073>⚠️</emoji> Анимация имени уже выключена.")
-        else:
-            await utils.answer(message, "<emoji document_id=5447644880824181073>⚠️</emoji> Анимация имени выключена.")
-            self.aname = False
+    @loader.command(alias="stopaname", ru_doc="""(stopaname) Выключить анимацию имени""")
+    async def stopanimatedname(self, message: Message):
+        """(stopaname) Turn off name animation"""
+        if self.aname is False:
+            return await utils.answer(message, self.strings("name_is_disabled"))
+        await utils.answer(message, self.strings("name_turned_off"))
+        self.aname = False
 
-    @loader.command(alias="astopbio")
-    async def animatedstopbio(self, message: Message):
-        """(astopbio) Выключить анимацию"""
-        if not self.abio:
-            return await utils.answer(message, "<emoji document_id=5447644880824181073>⚠️</emoji> Анимация био уже выключена.")
-        else:
-            await utils.answer(message, "<emoji document_id=5447644880824181073>⚠️</emoji> Анимация био выключена.")
-            self.abio = False
+    @loader.command(alias="stopabio", ru_doc="""(stopabio) Выключить анимацию био""")
+    async def stopanimatedbio(self, message: Message):
+        """(stopabio) Turn off bio animation"""
+        if self.abio is False:
+            return await utils.answer(message, self.strings("bio_is_disabled"))
+        await utils.answer(message, self.strings("bio_turned_off"))
+        self.abio = False
