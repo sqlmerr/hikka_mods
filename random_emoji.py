bde854e2a5dd51b8c75f3e8b06e1c5ba5aca50da
sqlmerr <107789010+sqlmerr@users.noreply.github.com>
2024-03-03 22:05:14 +0300

    new module

---
+++ random_emoji.py
@@ -0,0 +1,36 @@
+"""
+░██████╗░██████╗░██╗░░░░░███╗░░░███╗███████╗██████╗░██████╗░
+██╔════╝██╔═══██╗██║░░░░░████╗░████║██╔════╝██╔══██╗██╔══██╗
+╚█████╗░██║██╗██║██║░░░░░██╔████╔██║█████╗░░██████╔╝██████╔╝
+░╚═══██╗╚██████╔╝██║░░░░░██║╚██╔╝██║██╔══╝░░██╔══██╗██╔══██╗
+██████╔╝░╚═██╔═╝░███████╗██║░╚═╝░██║███████╗██║░░██║██║░░██║
+╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝
+"""
+
+
+# meta banner: https://github.com/sqlmerr/sqlmerr/blob/main/assets/hikka_mods/sqlmerrmodules_randomemoji.png?raw=true
+# meta developer: @sqlmerr_m
+
+import requests
+
+from .. import loader, utils
+from hikkatl.tl.types import Message
+
+
+@loader.tds
+class RandomEmoji(loader.Module):
+    """Just random emojis"""
+    
+    strings = {"name": "RandomEmoji"}
+    
+    @loader.command()
+    async def random_emoji(self, message: Message):
+        """Random emoji"""
+        url = "https://emojihub.yurace.pro/api/random"
+        emoji = await utils.run_sync(requests.get, url)
+        emoji = emoji.json()
+        
+        await utils.answer(
+            message,
+            "".join(html for html in emoji["htmlCode"])
+        )
