fee6866e9f5d0a9924548a4edc90940c6fa5d50f
sqlmerr <107789010+sqlmerr@users.noreply.github.com>
2024-03-27 21:50:30 +0300

    Add files via upload

---
+++ codeformat.py
@@ -0,0 +1,42 @@
+"""
+░██████╗░██████╗░██╗░░░░░███╗░░░███╗███████╗██████╗░██████╗░
+██╔════╝██╔═══██╗██║░░░░░████╗░████║██╔════╝██╔══██╗██╔══██╗
+╚█████╗░██║██╗██║██║░░░░░██╔████╔██║█████╗░░██████╔╝██████╔╝
+░╚═══██╗╚██████╔╝██║░░░░░██║╚██╔╝██║██╔══╝░░██╔══██╗██╔══██╗
+██████╔╝░╚═██╔═╝░███████╗██║░╚═╝░██║███████╗██║░░██║██║░░██║
+╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝
+"""
+
+# meta developer: @sqlmerr_m
+# meta banner: https://github.com/sqlmerr/sqlmerr/blob/main/assets/hikka_mods/sqlmerrmodules_codeformat.png?raw=true
+
+
+from .. import loader, utils
+
+from hikkatl.tl.types import Message
+
+
+@loader.tds
+class CodeFormat(loader.Module):
+    """Format your code!"""
+
+    strings = {"name": "CodeFormat"}
+
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "language",
+                "python",
+                validator=loader.validators.String()
+            )
+        )
+
+    @loader.command()
+    async def code(self, message: Message):
+        args = utils.get_args_raw(message)
+        language = self.config["language"]
+
+        await utils.answer(
+            message,
+            f"<pre><code class='language-{language}'>{args}</code></pre>"
+        )
