130a08c037604a972d3c51997bdff2c3d7989159
sqlmerr <107789010+sqlmerr@users.noreply.github.com>
2024-04-07 19:13:53 +0300

    Update silentmessages.py

--- silentmessages.py
+++ silentmessages.py
@@ -78,8 +78,8 @@ class SilentMessages(loader.Module):
             return
 
         if (
-            (message.chat and message.chat.id in self.config["chats"])
-            or (message.sender and message.sender.id in self.config["chats"])
+            (getattr(message, "sender") and message.chat.id in self.config["chats"])
+            or (getattr(message, "sender") and message.sender.id in self.config["chats"])
         ) and message.silent is True:
             link = await utils.get_message_link(message)
             chat_id = utils.get_chat_id(message)
